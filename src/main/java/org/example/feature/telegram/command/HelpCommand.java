819e02c99097a18c079a9f7db5b6896ec115d26e
LubaPika <luba.pika@gmail.com>
2024-03-21 19:22:59 +0200

    add my progect

---
+++ src/main/java/org/example/feature/telegram/command/HelpCommand.java
@@ -0,0 +1,27 @@
+package org.example.feature.telegram.command;
+
+import org.telegram.telegrambots.extensions.bots.commandbot.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Chat;
+import org.telegram.telegrambots.meta.api.objects.User;
+import org.telegram.telegrambots.meta.bots.AbsSender;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class HelpCommand extends BotCommand {
+    public HelpCommand() {
+        super("help", "Help command");
+    }
+
+    @Override
+    public void execute(AbsSender absSender, User user, Chat chat, String[] strings) {
+        SendMessage helpText = new SendMessage();
+        helpText.setText("Добро пожаловать");
+        helpText.setChatId(Long.toString(chat.getId()));
+
+        try {
+            absSender.execute(helpText);
+        }catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
